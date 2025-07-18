c61d4d97b368a02b1d2c1db74bdf200d4df23213
danielogabi <gabionisii@yahoo.com>
2025-07-18 03:04:58 +0300

    Revert "Workflow updates for online visibillity"
    This reverts commit 2d74b6e066e9e34c9857831a1077374b5dc2b43b.

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/helldivers-react-ts-learning/' : '/', // Correct base for GitHub Pages
   server: {
-    port: 5173, // Local development server port
-  },
+    port: 5173
+  }
 });
