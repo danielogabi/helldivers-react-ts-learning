66175544374d274f12591a9bd55cec7c92795d9a
danielogabi <gabionisii@yahoo.com>
2025-07-18 03:18:46 +0300

    Build Updates

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import PlanetsInfo from './pages/PlanetsInfo';
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename="/helldivers-react-ts-learning">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
